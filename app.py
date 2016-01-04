c830cb6ae0cb563491ff8023dd4e3554ef05b481
Mark Daniels <mark_daniels1982@hotmail.com>
2016-01-04 13:09:26 -0800

    custom error handling

--- app.py
+++ app.py
@@ -25,6 +25,14 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+  return render_template('500.html'), 500
+
 
 #run application instance
 if __name__ == '__main__':
