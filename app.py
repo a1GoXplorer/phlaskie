ec35b9d53c9029b1a8b8f4e0d15d8645ec2570e6
Mark Daniels <mark_daniels1982@hotmail.com>
2016-01-11 16:12:32 -0800

    make flashes work

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
-#import dependencies
-from datetime import datetime
-from flask import Flask, request, render_template
+#import modules
+# from datetime import datetime
+from flask import Flask, request, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
-
+from flask_wtf import Form
+from wtforms.fields import StringField, SubmitField
+from wtforms.validators import Required
 
 #create application object instance
 app = Flask(__name__)
+#configure encryption key
+app.config['SECRET_KEY'] = 'OpenSesame'
 
 #application instance passed to the constructor
 manager = Manager(app)
 moment = Moment(app)
 bootstrap = Bootstrap(app)
+#defines class for form field input
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
-# @app.route('/')
-# def index():
-#     user_agent = request.headers.get('User-Agent')
-#     return '<p>Your browser is %s</p>' % user_agent
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
-# @app.route('/index')
-# def index():
-#     return render_template('index.html')
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
